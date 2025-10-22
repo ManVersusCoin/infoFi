493729156caba0dd9458660a1bf35c37fcb33278
ManVersusCoin <98884815+ManVersusCoin@users.noreply.github.com>
2025-10-22 13:29:15 +0000

    Fix PackGenerator page

--- src/pages/xeet/XeetPackGenerator.tsx
+++ src/pages/xeet/XeetPackGenerator.tsx
@@ -1,4 +1,4 @@
-﻿import React, { useEffect, useState, useMemo } from "react";
+﻿import { useEffect, useState } from "react";
 import PackGenerator from "../../components/xeet/PackGenerator";
 
 interface Profile {
