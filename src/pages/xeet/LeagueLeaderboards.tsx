efcd803663a7f213bdbc7a6edafd8b7026f36fff
ManVersusCoin <98884815+ManVersusCoin@users.noreply.github.com>
2025-10-27 17:00:39 +0000

    Fix jsx issue.

--- src/pages/xeet/LeagueLeaderboards.tsx
+++ src/pages/xeet/LeagueLeaderboards.tsx
@@ -1,4 +1,5 @@
-﻿import { useEffect, useMemo, useRef, useState } from "react";
+﻿import * as React from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 
 /*
   LeagueLeaderboards.tsx
